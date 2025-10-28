2a9d6bfd33691ffad4b10ab6a788dc2580627b93
safiullah63 <safiullahkarimi.dev@gmail.com>
2025-10-28 08:50:54 +0430

    Project based installed

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title></title>
+</head>
+<body>
+    <div style="height: 100vh; display: flex ; justify-content: center; align-items: center">
+            <h1 style="text-align: center ; font-size: 150px ; color: rebeccapurple "  >پنــل مبايــــل فــــــــروشــــی</h1>
+    </div>
+</body>
+</html>
