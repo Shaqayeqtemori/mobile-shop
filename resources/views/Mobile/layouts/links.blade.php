5db1b3560912aae7ed10d860ccd8a25dbd36bfd5
Rokhshanaah <rokhshana.ah2024@gmail.com>
2025-11-10 10:14:04 +0430

    dashboard

--- resources/views/Mobile/layouts/links.blade.php
+++ resources/views/Mobile/layouts/links.blade.php
@@ -3,6 +3,9 @@
   <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
   <link rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"/>
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-        
-  
+        <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+ 
+<!-- Add the Font Awesome library -->
+<script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+ <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+  <link href="https://cdn.jsdelivr.net/npm/tailwindcss@3.3.2/dist/tailwind.min.css" rel="stylesheet">
